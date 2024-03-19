0028995c7c58375bcd47839423894f0c5a73750a
Carlos Hernandez <carlosjulioh200420@gmail.com>
2024-03-19 10:45:59 -0500

    feat: add services for controllers

--- src/api/tasks/task.controller.ts
+++ src/api/tasks/task.controller.ts
@@ -1,21 +1,47 @@
 import { Request, Response } from 'express';
+import taskService from './task.service';
 
 export async function getTasks(req: Request, res: Response): Promise<Response> {
-  return res.send('getTasks');
+  try {
+    const tasks = await taskService.getAllTasks();
+    return res.send(tasks);
+  } catch (error) {
+    return res.send(error);
+  }
 }
 
 export async function getTask(req: Request, res: Response): Promise<Response> {
-  return res.send('getTask');
+  try {
+    const task = await taskService.getTaskById(parseInt(req.params.id));
+    return res.send(task);
+  } catch (error) {
+    return res.send(error);
+  }
 }
 
 export async function createTask(req: Request, res: Response): Promise<Response> {
-  return res.send('createTask');
+  try {
+    const task = await taskService.createTask(req.body);
+    return res.send(task);
+  } catch (error) {
+    return res.send(error);
+  }
 }
 
 export async function updateTask(req: Request, res: Response): Promise<Response> {
-  return res.send('updateTask');
+  try {
+    const task = await taskService.updateTaskById(parseInt(req.params.id), req.body);
+    return res.send(task);
+  } catch (error) {
+    return res.send(error);
+  }
 }
 
 export async function deleteTask(req: Request, res: Response): Promise<Response> {
-  return res.send('deleteTask');
+  try {
+    const task = await taskService.deleteTaskById(parseInt(req.params.id));
+    return res.send(task);
+  } catch (error) {
+    return res.send(error);
+  }
 }

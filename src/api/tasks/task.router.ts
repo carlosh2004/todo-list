50a60b1fa396d5c67f71e93dabb936cd836d72c9
Carlos Hernandez <carlosjulioh200420@gmail.com>
2024-03-19 15:05:36 -0500

    feat: add validation middleware for tasks

--- src/api/tasks/task.router.ts
+++ src/api/tasks/task.router.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 import { createTask, deleteTask, getTask, getTasks, updateTask } from './task.controller';
+import { validate } from '../../middlewares/validate';
+import { insertTaskSchema } from './schema';
 
 const router = Router();
 
 router.get('/', getTasks);
 router.get('/:id', getTask);
-router.post('/', createTask);
-router.put('/:id', updateTask);
+router.post('/', validate(insertTaskSchema), createTask);
+router.put('/:id', validate(insertTaskSchema.partial()), updateTask);
 router.delete('/:id', deleteTask);
 
 export default router;

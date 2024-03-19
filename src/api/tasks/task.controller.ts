79ce22965252debc0bcc6e8fc755846dcc3e84cd
Carlos Hernandez <carlosjulioh200420@gmail.com>
2024-03-19 11:22:46 -0500

    feat: add response for controllers

--- src/api/tasks/task.controller.ts
+++ src/api/tasks/task.controller.ts
@@ -1,47 +1,49 @@
 import { Request, Response } from 'express';
+
 import taskService from './task.service';
+import response from '../../utils/response';
 
 export async function getTasks(req: Request, res: Response): Promise<Response> {
   try {
     const tasks = await taskService.getAllTasks();
-    return res.send(tasks);
+    return response.success(res, tasks);
   } catch (error) {
-    return res.send(error);
+    return response.error(res, 'Error processing request', 400, error);
   }
 }
 
 export async function getTask(req: Request, res: Response): Promise<Response> {
   try {
     const task = await taskService.getTaskById(parseInt(req.params.id));
-    return res.send(task);
+    return response.success(res, task || []);
   } catch (error) {
-    return res.send(error);
+    return response.error(res, 'Error processing request', 400, error);
   }
 }
 
 export async function createTask(req: Request, res: Response): Promise<Response> {
   try {
     const task = await taskService.createTask(req.body);
-    return res.send(task);
+    return response.success(res, task);
   } catch (error) {
-    return res.send(error);
+    return response.error(res, 'Error processing request', 400, error);
   }
 }
 
 export async function updateTask(req: Request, res: Response): Promise<Response> {
   try {
     const task = await taskService.updateTaskById(parseInt(req.params.id), req.body);
-    return res.send(task);
+    return response.success(res, task);
   } catch (error) {
-    return res.send(error);
+    return response.error(res, 'Error processing request', 400, error);
   }
 }
 
 export async function deleteTask(req: Request, res: Response): Promise<Response> {
   try {
     const task = await taskService.deleteTaskById(parseInt(req.params.id));
-    return res.send(task);
+    return response.success(res, task);
   } catch (error) {
-    return res.send(error);
+    return response.error(res, 'Error processing request', 400, error);
   }
 }
